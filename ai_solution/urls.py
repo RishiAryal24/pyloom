35efa6cd506c53f5ccbd0b45708f698a17c4addb
Rishi Aryal <aryalrishi29@gmail.com>
2026-07-01 20:27:20 +0545

    SEO improvements: Added sitemap support to enhance search engine indexing and visibility.

--- ai_solution/urls.py
+++ ai_solution/urls.py
@@ -4,6 +4,25 @@ from core import views  # Import views from the correct app
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.contrib.sitemaps.views import sitemap
+from core.sitemaps import (
+    ArticleSitemap,
+    SolutionSitemap,
+    EventSitemap,
+    ProjectSitemap,
+    TrainingSitemap,
+    StaticPageSitemap,
+)
+
+# Sitemap configuration
+sitemaps = {
+    'articles': ArticleSitemap,
+    'solutions': SolutionSitemap,
+    'events': EventSitemap,
+    'projects': ProjectSitemap,
+    'trainings': TrainingSitemap,
+    'static': StaticPageSitemap,
+}
 
 urlpatterns = [
     path('health/', views.health_check, name='health_check'),
@@ -12,6 +31,11 @@ urlpatterns = [
     path('', views.home, name='home'),  # Home page URL for 'Back to site'
     path('admin/password-reset/', views.password_reset_request, name='admin_password_reset'),
     path('tinymce/', include('tinymce.urls')),  # TinyMCE URLs for rich text editing
+    
+    # Sitemap URLs
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap-<section>.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    
     path('', include('core.urls')),  # Include core app URLs
     path('api/', include('core.api.urls')),
 ]
