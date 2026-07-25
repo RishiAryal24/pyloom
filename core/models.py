81301b517cc3178665b278d2188ab617e7dc533b
Rishi Aryal <aryalrishi15@gmail.com>
2026-07-25 11:45:44 +0545

    Event Models update to incorporate error

--- core/models.py
+++ core/models.py
@@ -355,6 +355,7 @@ class Event(models.Model):
     is_promoted = models.BooleanField(default=False)
     registration_url = models.URLField(blank=True)
     canonical_url = models.URLField(max_length=200, blank=True, null=True, default='')
+    meta_description = models.TextField(blank=True, default='')
     slug = models.SlugField(max_length=200, blank=True, unique=True)  # Ensure unique slug
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
